13ef1a277a0549bf5b990e4c1e8ee108e6ee74cf
hassannazir <hassan.nazir1998@gmail.com>
2023-02-04 12:26:37 +0500

    update

--- config/database.js
+++ config/database.js
@@ -4,13 +4,10 @@ const MongoClient = mongodb.MongoClient;
 let _db;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect(
-    "mongodb+srv://batman_123:nfT3Hyba@cluster0.emnwh.mongodb.net/shopping?authSource=admin&replicaSet=atlas-5gxz8j-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
-    {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  )
+  MongoClient.connect("", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
     .then((client) => {
       console.log("Connected!");
       _db = client.db();
